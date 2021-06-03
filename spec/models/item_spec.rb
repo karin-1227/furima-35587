d043ccbe18c01e5fd1cf26884f4093816902891b
karityan <karin141227@gmail.com>
2021-06-03 15:09:08 +0900

    修正２

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -67,10 +67,15 @@ RSpec.describe Item, type: :model do
         @item.valid?
         expect(@item.errors.full_messages).to include("Price is not a number")
       end
-      it '販売価格は、¥300~¥9,999,999の間でないと出品できない' do
+      it '販売価格は、300円未満だと出品できない' do
         @item.price = '100'
         @item.valid?
         expect(@item.errors.full_messages).to include("Price must be greater than 300")
+      end
+      it '販売価格は、10,000,000円以上だと出品できない' do
+        @item.price = '10000000'
+        @item.valid?
+        expect(@item.errors.full_messages).to include("Price must be less than 9999999")
       end
     end
   end
